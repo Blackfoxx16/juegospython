d23c910729bbea06e601636b12e179e9dda9c1dc
guadalupe.castillo <castillo.dev93@hotmail.com>
2023-08-10 16:28:16 -0600

    Se agrega la linea para llamar el juego de adivina binario

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from juegos.piedra_papel_tijeras import Piedra_papel_tijera
 from juegos.Ahorcado import Ahorcado
+from juegos.Busqueda_binaria import Busqueda_binaria as Adivina_numero
 
 # Jugar piedra papel o tijera
 
 ejecutando = True
 
-juegos = ["Piedra, papel o tijera",  "Ahorcado"]
-def jugar():
+juegos = ["Piedra, papel o tijera",  "Ahorcado", "Adivina el número"]
+def iniciar():
     # seleccionar juego
-    numero_de_juego = input("Elige tu juego: \n 1: Píedra, papel o tijera\n 2: Ahorcado \n 0: Salir \n")
+    numero_de_juego = input("Elige tu juego: \n 1: Píedra, papel o tijera\n 2: Ahorcado \n 3: Adivina el número \n 0: Salir \n")
 
     try:
         numero_de_juego = int(numero_de_juego)
@@ -22,11 +23,15 @@ def jugar():
         return
 
     if numero_de_juego == 1:
-        juego = Piedra_papel_tijera().jugar()
+        juego = Piedra_papel_tijera()
 
     if numero_de_juego == 2:
-        juego = Ahorcado().jugar()
+        juego = Ahorcado()
 
+    if numero_de_juego == 3:
+        juego = Adivina_numero()
+
+    juego.jugar()
 
 while(ejecutando):
-    jugar()
+    iniciar()
